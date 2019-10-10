e1610318c88a361e8be6812989df64441087215d
jurne <jurne.decadt@gmail.com>
2019-10-10 10:41:27 +0200

    get data from github

--- app/pages/profile/profile.js
+++ app/pages/profile/profile.js
@@ -1,4 +1,5 @@
 const observable = require('data/observable');
+const axios = require('axios');
 
 let user = observable.fromObject({
     id: 'jurne',
@@ -6,7 +7,7 @@ let user = observable.fromObject({
     nickname: 'unspecified',
     company: 'unemployed',
     followers: '0',
-    repositories: '0'
+    repositories: '0',    avatar: ''
 
 
 });
@@ -14,4 +15,25 @@ let user = observable.fromObject({
 exports.onLoaded = function(args) {
     let page = args.object;
     page.bindingContext = user;
+
+
+    // Make a request for a user with a given ID
+    axios.get('https://api.github.com/users/' + user.id)
+    .then(function (response) {
+        // handle success
+        user.name = response.data.login;
+        user.nickname = response.data.login;
+        user.repositories = response.data.public_repos;
+        user.followers = response.data.followers;
+        user.company = response.data.company;
+        user.avatar = response.data.avatar_url;
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
 };
